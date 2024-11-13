3d3a47b839a4c7bc44b34014a64088fdb7325cfb
Lakshmi1621 <koribillilaxmi39@gmail.com>
2024-11-13 07:59:49 -0800

    numbers pattern

---
+++ numbers pattern.java
@@ -0,0 +1,17 @@
+public class numbers1 {
+
+	public static void main(String[] args) {
+		int n=1;
+		// TODO Auto-generated method stub
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i+1;j++) {
+				System.out.print(n+" ");
+				n++;
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
