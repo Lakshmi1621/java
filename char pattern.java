57152a521a865da80f664ef86493b4f1f8c69c61
Lakshmi1621 <koribillilaxmi39@gmail.com>
2024-11-18 20:39:18 +0530

    Add files via upload

---
+++ char pattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class charpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		//char c='A';
+      for(int i=0;i<5;i++) {
+    	  char c='A';
+    	  for(int j=0;j<=i;j++) {
+    		  System.out.print(c);
+    		  c++;
+    	  }
+    	  System.out.println();
+      }
+	}
+
+}
